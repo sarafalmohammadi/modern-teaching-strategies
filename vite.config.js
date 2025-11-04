22c9c753ce52fc9da49933a1ad0b1cb6353ef3ca
sarafalmohammadi <sara.falmohammadi@gmail.com>
2025-11-04 20:48:37 +0300

    fix: base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ تعديل المسار الأساسي لحل مشكلة الصفحة البيضاء
 export default defineConfig({
+  base: './',
   plugins: [react()],
 })
