6de7a5ab1097b7303073333de5d2af59528f8fa8
sarafalmohammadi <sara.falmohammadi@gmail.com>
2025-11-05 00:16:17 +0300

    fix: working version ready for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { HashRouter } from 'react-router-dom'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { HashRouter } from "react-router-dom"
+import App from "./App"
+import "./styles/index.css"
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
 )
-
-
