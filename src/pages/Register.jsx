cc44fa10f293e7225e09b58a24f17653407728b5
sarafalmohammadi <sara.falmohammadi@gmail.com>
2025-11-15 23:11:35 +0300

    latest update - final fixes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -66,7 +66,7 @@ export default function Register() {
         />
         <input
           className="w-full border rounded px-3 py-2"
-          placeholder="البريد الجامعي"
+          placeholder="البريد الإلكتروني"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
